5422f59ca61f06f71da10c71cab0184e2409f1a0
gamermine <gamermine@dwightstudio.fr>
2026-09-10 02:47:35 +0200

    Adding some click handlers

--- src/gui/page/PlayerSetup.h
+++ src/gui/page/PlayerSetup.h
@@ -11,6 +11,12 @@ public:
         _cards.reserve(sizeof(BUZZERS_GPIO));
         ST7796S::addWidget(_lblInstr);
         ST7796S::_drawFilledRectangle({0, _lblInstr.rect().end.y + 10, ST7796S_DISP_WIDTH - 1, _lblInstr.rect().end.y + 10 + 2}, WHITE);
+        ST7796S::addWidget(_btnConfirm);
+
+        _btnConfirm.setOnClick([] {
+            //TODO: Add StatPage
+            ST7796S::setPage(nullptr);
+        });
 
         for (int i = 0; i < sizeof(BUZZERS_GPIO); i++) {
             pinMode(BUZZERS_GPIO[i], INPUT_PULLUP);
@@ -26,17 +32,21 @@ public:
     void update() override {
         if (_pendingIndex != -1) {
             const uint8_t id = Buzzer::add(BUZZERS_GPIO[_pendingIndex], BUZZERS_LED_GPIO[_pendingIndex], nullptr);
-            //Buzzer::get(id)->enable();
 
             _cards.emplace_back(vector2_t{_lastCardPos.x + 10, _lastCardPos.y + _lblInstr.rect().end.y + 22}, "Joueur " + std::to_string(_pendingIndex), WHITE);
             _lastCardPos = {_cards.back().rect().end.x, 0};
             ST7796S::addWidget(_cards.back());
+            _cards.back().setOnClick([id] {
+                //TODO: Add edit player settings dialog
+                Serial.printf("Edit player %d\n", id);
+            });
             _pendingIndex = -1;
         }
     }
 
 private:
     inline static Label _lblInstr {{10, 10}, "Appuyez sur votre buzzer pour rejoindre la partie.", &adwaita_mono_15_font, WHITE, BLACK};
+    inline static Button _btnConfirm {{400, 320}, {}, GREEN, "Valider", &adwaita_mono_15_font};
     inline static std::vector<PlayerSettingsCard> _cards;
     inline static std::vector<int> _addedIndices;
     inline static vector2_t _lastCardPos = {};
@@ -49,7 +59,6 @@ private:
 
         detachInterrupt(digitalPinToInterrupt(BUZZERS_GPIO[i]));
         _pendingIndex = i;
-        Serial.printf("%d\n", i);
     }
 };
 
